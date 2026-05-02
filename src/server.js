59934947d55349c3d9452a75b7e689685ae0242d
omarrwafiick <wafickmo11@gmail.com>
2026-05-03 01:28:42 +0300

    project setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import app from './app.js'
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'http://localhost:';
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${URL}${PORT}`);
+  console.log(`Listening on ${URL}${PORT}`);
 });
