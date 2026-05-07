38d23b141a67e2f25cea1f4ca3650448cd6893ba
omarrwafiick <wafickmo11@gmail.com>
2026-05-07 21:33:50 +0300

    test email service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,12 +1,22 @@
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  secure: false,
+export const mailer = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+
+  port: Number(
+    process.env.EMAIL_PORT
+  ),
+
+  secure:
+    process.env.EMAIL_SECURE
+    === "true",
+
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user:
+      process.env.EMAIL_USER,
+
+    pass:
+      process.env.EMAIL_PASSWORD,
   },
 });
 
@@ -14,7 +24,7 @@ export const sendEmailService = async (from, to, subject, body) => {
   if (!to || !subject || !body)
     throw new Error('Missing required email fields');
 
-  await transporter.sendMail({
+  await mailer.sendMail({
     from: from || process.env.SMTP_FROM,
     to,
     subject,
